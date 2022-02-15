5facc6e058f641d7cc8b454c0a6997f157952fc5
Kevin GANEM <Ganemkevin@gmail.com>
2022-02-15 16:40:27 +0100

    overflow done

---
+++ src/components/Card.js
@@ -0,0 +1,41 @@
+// REACT
+import React from "react";
+
+class Card extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="p-2">
+          {/* <button
+            onClick={() => this.props.handleClick(this.props.movieSelected.id)}
+            className="col-3"
+          > */}
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src={`https://image.tmdb.org/t/p/w300${this.props.movieSelected.poster_path}`}
+              className="card-img-top"
+              alt="Poster"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{this.props.movieSelected.title}</h5>
+              <p
+                className="card-text overflow-auto"
+                style={{ height: "10rem" }}
+              >
+                {this.props.movieSelected.overview}
+              </p>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                {this.props.movieSelected.release_date}
+              </li>
+            </ul>
+          </div>
+          {/* </button> */}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Card;
--- src/pages/Popular.js
+++ src/pages/Popular.js
@@ -1,11 +1,39 @@
 // REACT
 import React from "react";
-// ROUTER
-import { Link } from "react-router-dom";
+// COMPONENT
+import Card from "../components/Card";
 
 class Popular extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5db36b700acda7185166a4e97a159b98"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        this.setState({ movies: res.results });
+      });
+  }
+
   render() {
-    return <h1>Popular</h1>;
+    return (
+      <>
+        <h1>Popular</h1>
+        <div className="d-flex flex-wrap justify-content-around">
+          {this.state.movies.map((movie) => (
+            <Card handleClick={() => {}} key={movie.id} movieSelected={movie} />
+          ))}
+        </div>
+      </>
+    );
   }
 }
 
