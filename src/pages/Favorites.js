0e0d3ccc235a2f8276a82fe62b4173b09eebfce1
Kevin GANEM <Ganemkevin@gmail.com>
2022-02-17 12:41:35 +0100

    trying to fix multiple keys in favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,10 +1,62 @@
 // REACT
 import React from "react";
-// ROUTER
+// COMPONENT
+import Card from "../components/Card";
 
 class Favorites extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+      favIDs: this.getStorage(),
+      isLoading: true,
+    };
+  }
+
+  getMovie(id) {
+    fetch(
+      `http://api.themoviedb.org/3/movie/${id}?api_key=5db36b700acda7185166a4e97a159b98`
+    )
+      .then((res) => res.json())
+      .then((movieId) => {
+        this.state.movies.push(movieId);
+        this.setState({
+          movies: this.state.movies,
+          isLoading: false,
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.state.favIDs.forEach((movie) => this.getMovie(movie));
+  }
+
+  getStorage() {
+    return JSON.parse(localStorage.getItem("favorites")) || [];
+  }
+
   render() {
-    return <h1>Favorites</h1>;
+    return (
+      <div>
+        <h1 className="text-center">Favorites</h1>
+        <div className="d-flex flex-wrap justify-content-around">
+          {this.state.isLoading ? (
+            <div className="spinner-border text-primary" role="status" />
+          ) : (
+            <>
+              {this.state.movies.map((movie) => (
+                <Card
+                  handleClick={() => {}}
+                  key={movie.id}
+                  movieSelected={movie}
+                />
+              ))}
+            </>
+          )}
+        </div>
+      </div>
+    );
   }
 }
 
